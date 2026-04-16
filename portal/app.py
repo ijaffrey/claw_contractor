c13cb465cc240f787e185db3de02d3ffef649f6a
ijaffrey <ijaffrey@gmail.com>
2026-04-15 23:37:16 -0400

    [partial] step_2 action 5: append_file: portal/app.py

--- portal/app.py
+++ portal/app.py
@@ -60,3 +60,7 @@ def api_leads_summary():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+@app.route('/leads')
+def leads():
+    """Display leads in a table format"""
+    return render_template('leads.html')
